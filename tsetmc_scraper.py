330e54c9cbe7ccc4e2a12f0289ed793a95d956f3
Aghpour <76126008+Aghpour@users.noreply.github.com>
2022-09-11 20:00:06 +0430

    Update tsetmc_scraper.py

--- tsetmc_scraper.py
+++ tsetmc_scraper.py
@@ -1,9 +1,7 @@
 import os
 import pandas as pd
 import requests
-from bs4 import BeautifulSoup
 import time
-import ast
 import json
 from ratelimit import limits, RateLimitException, sleep_and_retry
 
@@ -11,49 +9,45 @@ start = time.time()
 ####################################
 # extract industry code
 url = 'http://cdn.tsetmc.com/api/StaticData/GetStaticData'
-
 payload = ''
 headers = {'user-agent': "Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/105.0.0.0 Safari/537.36"}
-
 response = requests.request("GET", url, data=payload, headers=headers)
 data = json.loads(response.text)['staticData']
 
-industry_code = {}
+industry_codes = []
 for i in data:
     typee = i['type']
-    name = i['name'].strip().replace('\u200c', '')
     code = i['code']
     if typee == 'IndustrialGroup':
-        industry_code[name] = code
+        industry_codes.append(code)
         
-urls = []
-for key, value in industry_code.items():
-    url = f'http://cdn.tsetmc.com/api/ClosingPrice/GetRelatedCompany/{value}'
-    urls.append(url)
+industry_urls = []
+for code in industry_codes:
+    industry_url = f'http://cdn.tsetmc.com/api/ClosingPrice/GetRelatedCompany/{code}'
+    industry_urls.append(industry_url)
 
 # loop through industries urls
+headers = {'user-agent': "Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/105.0.0.0 Safari/537.36"}
 tickers_code = []
-tickers = {}
-for url in urls:
-    response = requests.request("GET", url, data=payload, headers=headers)
-    ticker_data = json.loads(response.text)['relatedCompany']
-    for i in ticker_data:
-        insCode = i['instrument']['insCode']
-        ticker_name = i['instrument']['lVal18AFC']
-        tickers_code.append(insCode)
-        #tickers[ticker_name] = insCode
+for industry_url in industry_urls:
+    response = requests.request("GET", industry_url, data=payload, headers=headers)
+    industry_tickers = json.loads(response.text)['relatedCompany']
+    for company in industry_tickers:
+        insCode = company['instrument']['insCode']
+        symbol = company['instrument']['lVal18AFC']
+        if any(char.isdigit() for char in symbol): # rule out debt instruments
+            pass
+        else:
+            tickers_code.append(insCode)
 ####################################
-        
-headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/103.0.0.0 Safari/537.36'}
 dict_ticker = {}
-shareholders = []
 interval = 4
 max_calls = 6
 
 @sleep_and_retry
 @limits(calls=max_calls, period=interval)
-def get_ticker(item):
-    url_identity = f'http://cdn.tsetmc.com/api/Instrument/GetInstrumentIdentity/{item}'
+def get_ticker(insCode):
+    url_identity = f'http://cdn.tsetmc.com/api/Instrument/GetInstrumentIdentity/{insCode}'
     r = s.get(url_identity, headers=headers)
     identity_page = json.loads(r.text)  
     sector = identity_page['instrumentIdentity']['sector']['lSecVal']
@@ -65,22 +59,22 @@ def get_ticker(item):
     dict_ticker[en_instrument_id] = [sector, name, en_company_id, ticker, exchange]
     os.system('cls')
     print(i, ticker)
-    
-    
-def shareholder(item):  
-    url_shareholder = f'http://cdn.tsetmc.com/api/Shareholder/GetInstrumentShareHolderLast/{item}'
+
+shareholders = []
+def shareholder(insCode):  
+    url_shareholder = f'http://cdn.tsetmc.com/api/Shareholder/GetInstrumentShareHolderLast/{insCode}'
     r = s.get(url_shareholder, headers=headers)
     shareholder_page = json.loads(r.text)['shareHolder']
     df_shareholder = pd.DataFrame(shareholder_page)
     shareholders.append(df_shareholder)
 
 with requests.Session() as s:
-    for i, item in enumerate(tickers_code):
-        get_ticker(item)
+    for i, insCode in enumerate(tickers_code):
+        get_ticker(insCode)
         try:
-            shareholder(item)
+            shareholder(insCode)
         except:
-            print(f'shareholder passed: {item}')
+            print(f'shareholder passed: {insCode}')
         
 # info df
 df_info = pd.DataFrame.from_dict(dict_ticker, orient='index', columns=['sector','name', 'en_company_id', 'ticker', 'exchange'])
