c9fcbd1308fe1735da3143455c8b69b7fb667228
Aghpour <76126008+Aghpour@users.noreply.github.com>
2022-09-11 12:19:49 +0430

    Update tsetmc_scraper.py

--- tsetmc_scraper.py
+++ tsetmc_scraper.py
@@ -4,34 +4,58 @@ import requests
 from bs4 import BeautifulSoup
 import time
 import ast
+import json
 from ratelimit import limits, RateLimitException, sleep_and_retry
 
 start = time.time()
-# find all listed tickers
-url_marketwatch = 'http://tsetmc.com/tsev2/data/MarketWatchPlus.aspx'
-r = requests.get(url_marketwatch)
-soup = BeautifulSoup(r.content, 'html.parser')
-data = soup.text.split(';')
+####################################
+# extract industry code
+url = 'http://cdn.tsetmc.com/api/StaticData/GetStaticData'
 
-# extract tickers code's, E.g.: http://www.tsetmc.com/Loader.aspx?ParTree=151311&i={35425587644337450}
-tickers_codes = []
-for index, item in enumerate(data):
-    if len(data[index].split(',')) == 8:
-        tickers_codes.append(data[index].split(',')[0])
-tickers_codes_unique = list(set(tickers_codes))
+payload = ''
+headers = {'user-agent': "Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/105.0.0.0 Safari/537.36"}
+
+response = requests.request("GET", url, data=payload, headers=headers)
+data = json.loads(response.text)['staticData']
+
+industry_code = {}
+for i in data:
+    typee = i['type']
+    name = i['name'].strip().replace('\u200c', '')
+    code = i['code']
+    if typee == 'IndustrialGroup':
+        industry_code[name] = code
+        
+urls = []
+for key, value in industry_code.items():
+    url = f'http://cdn.tsetmc.com/api/ClosingPrice/GetRelatedCompany/{value}'
+    urls.append(url)
+
+# loop through industries urls
+tickers_code = []
+tickers = {}
+for url in urls:
+    response = requests.request("GET", url, data=payload, headers=headers)
+    ticker_data = json.loads(response.text)['relatedCompany']
+    for i in ticker_data:
+        insCode = i['instrument']['insCode']
+        ticker_name = i['instrument']['lVal18AFC']
+        tickers_code.append(insCode)
+        #tickers[ticker_name] = insCode
+####################################
         
 headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/103.0.0.0 Safari/537.36'}
 dict_ticker = {}
 shareholders = []
 interval = 4
-max_calls = 10
+max_calls = 6
 
 @sleep_and_retry
 @limits(calls=max_calls, period=interval)
 def get_ticker(item):
     url_identity = f'http://cdn.tsetmc.com/api/Instrument/GetInstrumentIdentity/{item}'
     r = s.get(url_identity, headers=headers)
-    identity_page = ast.literal_eval(r.text)  
+    identity_page = json.loads(r.text)  
     sector = identity_page['instrumentIdentity']['sector']['lSecVal']
     en_instrument_id = identity_page['instrumentIdentity']['instrumentID']  # unique
     name = identity_page['instrumentIdentity']['lSoc30']
@@ -46,12 +70,12 @@ def get_ticker(item):
 def shareholder(item):  
     url_shareholder = f'http://cdn.tsetmc.com/api/Shareholder/GetInstrumentShareHolderLast/{item}'
     r = s.get(url_shareholder, headers=headers)
-    shareholder_page = ast.literal_eval(r.text)['shareHolder']
+    shareholder_page = json.loads(r.text)['shareHolder']
     df_shareholder = pd.DataFrame(shareholder_page)
     shareholders.append(df_shareholder)
 
 with requests.Session() as s:
-    for i, item in enumerate(tickers_codes_unique):
+    for i, item in enumerate(tickers_code):
         get_ticker(item)
         try:
             shareholder(item)
